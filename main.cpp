d667c147e6d31deee34fc7ca8646976b2570aad5
freddielumsden <freddielumsden@gmail.com>
2022-02-08 15:21:54 +0000

    Improve Class

--- main.cpp
+++ main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
+#include <list>
 
+class Student
+{
+public:
+    std::list<std::string> subjects;
+    Student(std::list<std::string> _subjects)
+    {
+        subjects = _subjects;
+    }
+    std::list<std::string> getSubjects()
+    {
+        return subjects;
+    }
+    void setSubjects(std::list<std::string> _subjects)
+    {
+        subjects = _subjects;
+    }
+    void addSubject(std::string subject)
+    {
+        subjects.push_back(subject);
+    }
+    void removeSubject
+};
 int main()
 {
-    std::cout << "hello";
+    Student student1({"Maths", "English"});
+    student1.addSubject("Science");
+    std::list<std::string> subjects = student1.getSubjects();
+    for (auto subject : subjects) std::cout << subject << std::endl;
     return 0;
 }
